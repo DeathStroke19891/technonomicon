f074e746e97057301d1e84fd5ccbaa4665b7af4b
Sridhar D Kedlaya <sridhardked@gmail.com>
2026-02-21 21:52:06 +0530

    chore: Update to latest deps and pnpm

--- flake.nix
+++ flake.nix
@@ -1,16 +1,43 @@
 {
-  description = "SDL2 tutorial";
+  description = "technonomicon";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-  let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in
-  {
-    devShells.${system}.default = (import ./shell.nix { inherit pkgs;});
-  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        shell = import ./shell.nix { inherit pkgs; };
+      in {
+        devShells.${system}.default = shell;
+
+        packages.default = pkgs.stdenv.mkDerivation rec {
+          pname = "technonomicon";
+          version = "0.0.1";
+          src = ./.;
+
+          nativeBuildInputs = shell.nativeBuildInputs ++ [
+            pkgs.pnpmConfigHook
+          ];
+
+          pnpmDeps = pkgs.fetchPnpmDeps {
+            inherit pname version src;
+            hash = "sha256-vTagDXjIieRtHDjzOvi0V4VNVGJf1xfnyidNYaJQAkc=";
+            fetcherVersion = 1;
+          };
+
+          buildPhase = ''
+            pnpm run build
+          '';
+
+          installPhase = ''
+            cp -r dist $out
+          '';
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+      });
 }
